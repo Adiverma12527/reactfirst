af1477820078f4922c08d8dfec208bf44e6a832c
aditya verma <adityavermasuper.12527@gmail.com>
2023-04-21 08:13:21 +0530

    react 3day assignment

--- src/App.js
+++ src/App.js
@@ -1,37 +1,11 @@
 import './App.css';
-import { Button } from './components/button/button';
-import Card from './components/card/card';
-
+import { Profile } from './props';
 function App() {
-
-  //  components -/> small isolated piece of code of you complete ui .
-  // components  -/> functions (react function)
-  //  js function -/> takes datatype as input and return data types
-  // react function -/> takes props as input and return jsx / html likw components .
-  //  react functions startWith Capital letter only . 
-
-
-//   Jsx -/> javascript extension 
-//   it provides the power to write the html in js file 
-
-//  ui 
-//  functionality 
-//  data management 
-//  folder structure (Atomic folder structure)
-
-
-//  css  -> style to style the component
-//  js -/> code to ceate the component 
-
-
-
   return (
-    <div className="App">
-      <h1>Welcome to react js  ....</h1>
-      <Button/>
-      <Card/>
+        <div className="App">
+        <Profile text="Birthday party invitation" name="jaGdish" birth="birthday party" friend="Ritu,Saurabh,Kartik." field="Green field Avenue. " last="jaGdish"/>
     </div>
+
   );
 }
-
 export default App;
