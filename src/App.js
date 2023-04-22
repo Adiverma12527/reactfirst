4fef0359600d719f9945856eb2805cc51644b1b4
aditya verma <adityavermasuper.12527@gmail.com>
2023-04-22 15:13:19 -0700

    iamhere

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Profile } from './props';
 function App() {
   return (
         <div className="App">
-        <Profile text="Birthday party invitation" name="jaGdish" birth="birthday party" friend="Ritu,Saurabh,Kartik." field="Green field Avenue. " last="jaGdish"/>
+        <Profile text="Birthday party invitation new new " name="jaGdish" birth="birthday party" friend="Ritu,Saurabh,Kartik." field="Green field Avenue. " last="jaGdish"/>
     </div>
 
   );
